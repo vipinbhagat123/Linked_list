88b0aeb8fc0deaeac28025612c3ad067d13f212c
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-05-30 12:12:13 +0530

    lenght of linkelist

---
+++ 5. length_of_linkedlist.py
@@ -0,0 +1,32 @@
+class node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+def print_ll(head):
+    temp = head
+    while (temp!=None):
+        print(temp.data, end='->')
+        temp =temp.next
+    print()
+    return 
+
+def take_input():
+    value = int(input("Enter the value of node:-"))
+    head= None
+    tail = None
+    while (value!=-1):
+        newNode = node(value)
+        if(head==None):
+            head = newNode
+        else:
+            temp = head
+            while (temp.next!=None):
+                temp = temp.next
+            temp.next = newNode
+        value = int(input("Enter the value of Node:-"))
+    return head
+newhead = take_input()
+print_ll(newhead)
+
+
